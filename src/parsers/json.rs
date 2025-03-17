fc06a302bccda006245fe7fa90e9c57d6c2eeccd
Tomasz Bawor <bawortomasz@gmail.com>
2025-03-17 22:34:47 +1300

    chore: finish json parser

--- src/parsers/json.rs
+++ src/parsers/json.rs
@@ -1,3 +1,5 @@
+use std::{fs::File, io::Write, path::Path};
+
 use super::PalleteParser;
 
 pub struct JsonParser {}
@@ -9,7 +11,15 @@ impl PalleteParser for JsonParser {
     ) -> Result<(), Box<dyn std::error::Error>> {
         let json: Vec<String> = pallete.iter().map(String::from).collect();
 
-        println!("{:?}", json);
+        let result = serde_json::to_string_pretty(&json).unwrap();
+
+        if let Some(path) = out_file_path {
+            let mut file = File::create(Path::new(path))?;
+            file.write_all(result.as_bytes())?;
+        } else {
+            println!("{}", result);
+        }
+
         Ok(())
     }
 }
